9f77b58c4f606b2428c34bf0efeea9f348f3e830
U-OFFICE\Brandon <brandon.bonton@gmail.com>
2021-06-02 21:20:40 -0700

    clarified default keymap

--- keyboards/eyeohdesigns/humble40/keymaps/default/keymap.c
+++ keyboards/eyeohdesigns/humble40/keymaps/default/keymap.c
@@ -17,9 +17,9 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    DVORAK,
-	FUNCT,
-	NUMBRS
+    QWERTY,
+    FUNCT,
+    NUMBRS
 };
 
 // Defines the keycodes used by our macros in process_record_user
@@ -27,22 +27,22 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [DVORAK] = LAYOUT_625_proto(
-        KC_ESC,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,       KC_Y,       KC_F,   KC_G, KC_C,       KC_R,    KC_L,    KC_BSPC,\
-		KC_TAB,  KC_A,    KC_O,    KC_E,    KC_U,       KC_I,       KC_D,   KC_H, KC_T,       KC_N,           KC_S,     \
-        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,       KC_X,       KC_B,   KC_M, KC_W,       KC_V,    KC_Z,    KC_RSFT,\
-        KC_LCTL,                   KC_LALT,                   LT(NUMBRS,KC_SPC),   MO(FUNCT),                    KC_LGUI \
+    [QWERTY] = LAYOUT_U_2_A(
+        KC_ESC,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,    KC_P,      KC_BSPC, \
+        KC_TAB,   KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_ENTER,      \
+        KC_LSFT,   KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,\
+        KC_LCTL,    KC_LGUI, KC_LALT, MO(NUMBRS), KC_SPC,   MO(FUNCT),  KC_RALT,        KC_RCTL \
 ),
-    [FUNCT] = LAYOUT_625_proto(
-        KC_VOLU,  KC_QUOT, KC_PGUP, KC_DOT,  KC_P,       KC_SLSH, KC_BSLS,   KC_G, KC_UP,       KC_R,    KC_PSCR,    KC_DEL,\
-		KC_VOLD,  KC_HOME,    KC_PGDN,    KC_END,    KC_U,       KC_MINS, KC_EQL,   KC_LEFT, KC_DOWN,       KC_RIGHT,           KC_ENTER,     \
-        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_LBRC,       KC_X,       KC_RBRC,   KC_M, KC_W,       KC_V,    KC_Z,    KC_RSFT,\
-        KC_LCTL,                   KC_LALT,                   MO(NUMBRS),   KC_TRNS,                    RESET \
+    [FUNCT] = LAYOUT_U_2_A(
+        KC_VOLU,  KC_Q,    KC_PGUP, KC_DOT, KC_R,    KC_T,    KC_Y, KC_U,    KC_UP,   KC_O,    KC_PSCR,    KC_DEL,\
+        KC_VOLD,   KC_HOME, KC_PGDN, KC_END, KC_F,    KC_MINS, KC_EQL,  KC_LEFT, KC_DOWN, KC_RIGHT,  KC_QUOT,   KC_ENTER,     \
+        KC_LSFT,    KC_SCLN, KC_X,    KC_C,   KC_LBRC, KC_B,    KC_RBRC, KC_M,    KC_COMM,  KC_DOT,  KC_BSLS, KC_RSFT,\
+        KC_LCTL,      KC_LGUI, KC_LALT, MO(NUMBRS),           KC_SPC,   KC_TRNS,  KC_RALT,                   RESET \
 ),
-    [NUMBRS] = LAYOUT_625_proto(
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,\
-		KC_MUTE,  KC_1,    KC_2,    KC_3,    KC_4,       KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     \
-        KC_CAPS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,     KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,    KC_RSFT,\
-        KC_LCTL,                   KC_LALT,                   KC_SPC,   KC_TRNS,                    RESET \
+    [NUMBRS] = LAYOUT_U_2_A(
+        KC_F1,  KC_F2,   KC_F3, KC_F4,   KC_F5,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,       KC_F12,\
+        KC_MUTE, KC_1,    KC_2,  KC_3,    KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,        KC_ENTER,  \
+        KC_CAPS,  KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,    KC_RSFT,\
+        KC_LCTL,      KC_LGUI, KC_LALT,  KC_TRNS,           KC_SPC,   MO(FUNCT),  KC_RALT,                  KC_RCTL\
 ),
 };
